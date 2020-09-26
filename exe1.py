a7ae2ffe0b9f0ad363a9b93d1012c097e6da78e1
Apoorv181 <57178721+Apoorv181@users.noreply.github.com>
2020-09-26 20:45:58 +0530

    fourth commit

--- exe1.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import setup , Executable
-setup(name = "MY GAME",
-      version="1.0",
-      description = "adventure game"
-      executables = [Executable(r"D:\Softwares\Python\programs\game_v2.py")]
-     )
